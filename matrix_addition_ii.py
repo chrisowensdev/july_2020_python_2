101dd2e96ae9de942ac016630e53b161806c2cc9
Chris Owens <guitarrockstarco@gmail.com>
2020-08-09 21:07:30 -0400

    Still needs workd

---
+++ matrix_addition_ii.py
@@ -0,0 +1,12 @@
+X = [[1, 3], [2, 4]]
+Y = [[5, 2], [1, 0]]
+result = [[0, 0], [0, 0]]
+
+for i in range(len(X)):
+    for j in range(len(Y)):
+        result[i][j] = X[i][j] + Y[i][j]
+
+print(result)
+
+
+# Still need to work on
